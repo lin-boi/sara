a5ba71f8fbde03ebcd29a20040c1a438567fb17a
lin-boi <58958629+lin-boi@users.noreply.github.com>
2020-01-10 15:09:10 -0500

    Edited config.mk added definitions
    Added definitions for OpenBSD and X11R6

--- config.mk
+++ config.mk
@@ -1,1 +1,15 @@
-Sample config.mk file
+#Sara linboy fork Version
+VERSION=1.0
+
+#Customize to fit your system
+
+#paths
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+#Freetype
+
+#OpenBSD (Uncomment next line)
+#FREETYPEINC = ${X11INC}/freetype2
+
+
