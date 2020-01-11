3df3d09ec09ae0a6b7347bd22c4525bef48f5303
lin-boi <58958629+lin-boi@users.noreply.github.com>
2020-01-11 09:32:19 -0500

    Update config.mk added flags and libs
    Added flag and lib definitions, much credit to DWM's config.mk

--- config.mk
+++ config.mk
@@ -8,8 +8,16 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 #Freetype
-
-#OpenBSD (Uncomment next line)
+FREETYPELIBS = -lfontconfig -lXft
+FREETYPEINC = /usr/include/freetype2
+#OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
 
+#Includes and Libs
+INCS = -I${X11INC} -I${FREETYPEINC}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
+#Flags
+CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+DFLAGS  = ${LIBS}
 
